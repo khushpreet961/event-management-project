534d423034311eae5e5014cb6c2f1f2bb182fa02
khushpreet <khushp961@gmail.com>
2025-02-10 14:40:15 +0530

    updated

--- backend event/app.js
+++ backend event/app.js
@@ -19,6 +19,10 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/",(req,res)=>{
+  res.send("Hello from server");
+})
+
 app.use("/api/v1/message", messageRouter);
 
 dbConnection();
